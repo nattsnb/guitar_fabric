48c84d2a309541db446af1f5ba0706e024f90318
Natasza ASzatan <natiye.nb@gmai.com>
2024-02-05 13:03:07 +0000

    copiedagain to fix issues

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ class Guitar {
             this.isTuned === true &&
             this.isFaulty === false
         ) {
-            console.log("Guitar is playing just fine!")
+            console.log("guitar is playing just fine!")
             return true;
         } else {
             console.log("The guitar is not playable!")
@@ -41,17 +41,15 @@ class Guitar {
 }
 
 class Supplier {
-    constructor(frequency) {
+    constructor() {
         this.lastDeliveryDateTime = 0;
-        this.frequencyInSeconds = frequency;
     }
 
     delivery() {
-        let randomNumber = getRandomIntInclusive(3,6);
-        const randomArray = new Array(randomNumber)
-        console.log(randomArray)
+        let arrayLength = getRandomIntInclusive(3,6);
+        const randomArry = []
+        randomArry.length = arrayLength
     }
-    setTimeout(delivery, frequencyInSeconds*1000);
 }
 
 const neck = new Neck();
@@ -64,23 +62,23 @@ console.log(guitar);
 guitar.tune()
 console.log(guitar.isPlayable());
 
-const someDate = new Date("2024-01-01");
-console.log(someDate.toISOString());
-console.log(someDate.getTime());
-
-// random number (from 3 to 6)
-
-let iteration = 0;
-while(iteration < 20) {
-    let randomNumber = getRandomIntInclusive(3,6);
-    const randomArry = new Array(randomNumber)
-    console.log(randomArry)
-
-    iteration++;
-}
-
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
+// const someDate = new Date("2024-01-01");
+// console.log(someDate.toISOString());
+// console.log(someDate.getTime());
+//
+// // random number (from 3 to 6)
+//
+// let iteration = 0;
+// while(iteration < 20) {
+//     let randomNumber = getRandomIntInclusive(3,6);
+//     const randomArry = new Array(randomNumber)
+//     console.log(randomArry)
+//
+//     iteration++;
+// }
+//
+// function getRandomIntInclusive(min, max) {
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+// }
